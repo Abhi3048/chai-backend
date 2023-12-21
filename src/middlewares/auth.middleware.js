52d9d21dcbf611e3b25ac35ee612f0987c71941f
Abhi3048 <learn8600.me@gmail.com>
2023-12-21 22:42:36 +0530

    add refreshAccessTken endpoint

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,15 +6,17 @@ import { User } from "../models/user.model.js";
 export const verifyJWT = asyncHandler(async(req, _, next)=>{
    try {
      const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer","");
+     
      if(!token){
          throw new ApiError(401,"Unauthorized request");
      }
+
      const decodedToken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET);
  
      const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
  
      if(!user){
-         //NEXT_VIDEO: discuss about frontend
+         //NEXT_VIDEO: discuss about frontend✅
          throw new ApiError(401,"Invalid Access Token")
      }
  
